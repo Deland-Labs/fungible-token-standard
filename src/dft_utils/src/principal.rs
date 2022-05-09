a6f9e1527e3f71e2e8356daf0984a4be735f85ed
witter-deland <87846830+witter-deland@users.noreply.github.com>
2022-05-09 15:38:53 +0800

    Unit Test: dft_utils (#109)

--- src/dft_utils/src/principal.rs
+++ src/dft_utils/src/principal.rs
@@ -1,13 +1,17 @@
 #![allow(dead_code)]
+
 use candid::Principal;
 
 const CANISTER_ID_HASH_LEN_IN_BYTES: usize = 10;
+
 pub fn is_canister(id: &Principal) -> bool {
     let blob = id.as_slice();
     blob.len() == CANISTER_ID_HASH_LEN_IN_BYTES
 }
+
 const HASH_LEN_IN_BYTES: usize = 28;
 const TYPE_SELF_AUTH: u8 = 0x02;
+
 pub fn is_user_principal(id: &Principal) -> bool {
     let blob = id.as_slice();
     if blob.len() != HASH_LEN_IN_BYTES + 1 {
@@ -17,4 +21,30 @@ pub fn is_user_principal(id: &Principal) -> bool {
         return false;
     }
     true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use candid::Principal;
+
+    #[test]
+    fn test_is_canister() {
+        let canister_id = Principal::from_text("ryjl3-tyaaa-aaaaa-aaaba-cai").unwrap();
+        let principal_id =
+            Principal::from_text("zctcl-skicc-vgye3-xaoas-gt6d4-o2a54-v6era-nw6hm-bda6p-ur54p-oae")
+                .unwrap();
+        assert!(is_canister(&canister_id));
+        assert!(!is_canister(&principal_id));
+    }
+
+    #[test]
+    fn test_is_user_principal() {
+        let canister_id = Principal::from_text("ryjl3-tyaaa-aaaaa-aaaba-cai").unwrap();
+        let principal_id =
+            Principal::from_text("zctcl-skicc-vgye3-xaoas-gt6d4-o2a54-v6era-nw6hm-bda6p-ur54p-oae")
+                .unwrap();
+        assert!(!is_user_principal(&canister_id));
+        assert!(is_user_principal(&principal_id));
+    }
 }
