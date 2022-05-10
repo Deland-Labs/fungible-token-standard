c61c13bc75c32908be6aa8ea9802caa781f0f9b7
Varik Baker <varik1770@gmail.com>
2022-05-10 20:20:11 +0800

    Unit Test: dft_types token_response

--- src/dft_types/src/token_response.rs
+++ src/dft_types/src/token_response.rs
@@ -91,6 +91,7 @@ pub struct QueryBlocksResult {
 
 pub type TransactionList = Vec<Transaction>;
 pub type CandidTransactionList = Vec<CandidTransaction>;
+
 #[derive(CandidType, Debug, Clone)]
 pub enum TransactionResult {
     // Return tx if exist in the DFT cache txs
@@ -100,6 +101,7 @@ pub enum TransactionResult {
     // Such as out of tx index or tx id not exist
     Err(ErrorInfo),
 }
+
 #[derive(CandidType)]
 pub enum TransactionListResult {
     Ok(CandidTransactionList),
@@ -118,5 +120,74 @@ impl From<CommonResult<TransactionList>> for TransactionListResult {
             }
             Err(error) => TransactionListResult::Err(error.into()),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{CandidOperation, CandidTokenFee, Operation, TokenFee};
+
+    #[test]
+    fn test_transaction_list_common_result_to_transaction_list_result() {
+        let tx_list = vec![
+            Transaction {
+                operation: Operation::AddMinter {
+                    caller: "czjfo-ddpvm-6sibl-6zbox-ee5zq-bx3hc-e336t-s6pka-dupmy-wcxqi-fae"
+                        .parse()
+                        .unwrap(),
+                    minter: "qupnt-ohzy3-npshw-oba2m-sttkq-tyawc-vufye-u5fbz-zb6yu-conr3-tqe"
+                        .parse()
+                        .unwrap(),
+                },
+                created_at: 1,
+            },
+            Transaction {
+                operation: Operation::FeeModify {
+                    caller: "czjfo-ddpvm-6sibl-6zbox-ee5zq-bx3hc-e336t-s6pka-dupmy-wcxqi-fae"
+                        .parse()
+                        .unwrap(),
+                    new_fee: TokenFee {
+                        minimum: 1u32.into(),
+                        rate: 1u32.into(),
+                        rate_decimals: 8,
+                    },
+                },
+                created_at: 2,
+            },
+        ];
+
+        let result = CommonResult::Ok(tx_list);
+        let tx_list_res: TransactionListResult = result.clone().into();
+        match tx_list_res {
+            TransactionListResult::Ok(tx_list) => {
+                assert_eq!(tx_list.len(), 2);
+                assert_eq!(
+                    tx_list[0].operation,
+                    CandidOperation::AddMinter {
+                        caller: "czjfo-ddpvm-6sibl-6zbox-ee5zq-bx3hc-e336t-s6pka-dupmy-wcxqi-fae"
+                            .parse()
+                            .unwrap(),
+                        minter: "qupnt-ohzy3-npshw-oba2m-sttkq-tyawc-vufye-u5fbz-zb6yu-conr3-tqe"
+                            .parse()
+                            .unwrap(),
+                    }
+                );
+                assert_eq!(
+                    tx_list[1].operation,
+                    CandidOperation::FeeModify {
+                        caller: "czjfo-ddpvm-6sibl-6zbox-ee5zq-bx3hc-e336t-s6pka-dupmy-wcxqi-fae"
+                            .parse()
+                            .unwrap(),
+                        new_fee: CandidTokenFee {
+                            minimum: 1u32.into(),
+                            rate: 1u32.into(),
+                            rate_decimals: 8,
+                        },
+                    }
+                );
+            }
+            _ => panic!("should be ok"),
+        };
     }
 }
