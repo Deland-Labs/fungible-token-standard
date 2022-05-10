8480b286c5d0c0067ce125f5ca5924a02a9edc77
Varik Baker <varik1770@gmail.com>
2022-05-11 01:31:33 +0800

    Unit Test:dft_types token_transaction_window

--- src/dft_types/src/token_transaction_window.rs
+++ src/dft_types/src/token_transaction_window.rs
@@ -109,6 +109,17 @@ impl TokenTransactionWindow {
             {
                 return Err(DFTError::TooManyTransactionsInReplayPreventionWindow);
             }
+
+            assert!(
+                false,
+                "throttle check,{}  {}",
+                self.transactions_by_height
+                    .get(num_in_window.saturating_sub(max_rate))
+                    .map(|x| x.block_timestamp)
+                    .unwrap_or_else(|| 0)
+                    + 10u64.pow(9),
+                &now
+            );
         }
 
         Ok(())
@@ -145,5 +156,168 @@ impl StableState for TokenTransactionWindow {
             transactions_by_hash,
             transactions_by_height,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::convert::TryInto;
+
+    use dft_utils::sha256::compute_hash;
+
+    use super::*;
+
+    #[test]
+    fn test_token_transaction_window() {
+        let mut window = TokenTransactionWindow::new();
+        assert_eq!(window.transactions_by_height.len(), 0);
+        assert_eq!(window.transactions_by_hash.len(), 0);
+
+        let block_height = BigUint::from(5u32);
+        let block_timestamp = 168_000_000_000u64;
+        let tx_info: TransactionInfo = TransactionInfo {
+            block_timestamp: block_timestamp.clone(),
+            tx_hash: compute_hash("test".as_bytes()),
+        };
+        window.push_transaction(block_height.clone(), tx_info.clone());
+        assert_eq!(window.transactions_by_height.len(), 1);
+        assert_eq!(window.transactions_by_hash.len(), 1);
+        assert_eq!(
+            window
+                .transactions_by_height
+                .front()
+                .unwrap()
+                .block_timestamp,
+            block_timestamp
+        );
+        assert_eq!(
+            window.transactions_by_hash.get(&tx_info.tx_hash).unwrap(),
+            &block_height
+        );
+    }
+
+    #[test]
+    fn test_purge_old_transactions() {
+        let mut window = TokenTransactionWindow::new();
+
+        let block_height = BigUint::from(5u32);
+        // get now timestamp
+        let now: u64 = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_nanos()
+            .try_into()
+            .unwrap();
+        let block_timestamp = now - window.transaction_window - constants::PERMITTED_DRIFT - 1000;
+        let tx_info: TransactionInfo = TransactionInfo {
+            block_timestamp: block_timestamp.clone(),
+            tx_hash: compute_hash("test".as_bytes()),
+        };
+        window.push_transaction(block_height.clone(), tx_info.clone());
+        assert_eq!(window.transactions_by_height.len(), 1);
+        assert_eq!(window.transactions_by_hash.len(), 1);
+
+        let block_height = BigUint::from(6u32);
+        let block_timestamp = now;
+        let tx_info: TransactionInfo = TransactionInfo {
+            block_timestamp: block_timestamp.clone(),
+            tx_hash: compute_hash("test2".as_bytes()),
+        };
+        window.push_transaction(block_height.clone(), tx_info.clone());
+        assert_eq!(window.transactions_by_height.len(), 2);
+        assert_eq!(window.transactions_by_hash.len(), 2);
+
+        let removed = window.purge_old_transactions(now);
+        assert_eq!(removed, 1);
+        assert_eq!(window.transactions_by_height.len(), 1);
+        assert_eq!(window.transactions_by_hash.len(), 1);
+
+        let removed = window.purge_old_transactions(
+            now + window.transaction_window + constants::PERMITTED_DRIFT + 1,
+        );
+        assert_eq!(removed, 1);
+        assert_eq!(window.transactions_by_height.len(), 0);
+        assert_eq!(window.transactions_by_hash.len(), 0);
+    }
+
+    #[test]
+    fn test_throttle_check() {
+        let mut window = TokenTransactionWindow::new();
+        let max_txs_in_window = window.max_transactions_in_window();
+
+        let push_txs_count = max_txs_in_window / 2 - 1;
+
+        // get now timestamp
+        let now: u64 = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_nanos()
+            .try_into()
+            .unwrap();
+
+        for i in 0..push_txs_count {
+            let block_height = BigUint::from(i as u32);
+            let block_timestamp = now - push_txs_count as u64 + 1;
+            let tx_info: TransactionInfo = TransactionInfo {
+                block_timestamp: block_timestamp.clone(),
+                tx_hash: compute_hash(format!("test{}", i).as_bytes()),
+            };
+            window.push_transaction(block_height.clone(), tx_info.clone());
+
+            assert_eq!(window.transactions_by_height.len(), i + 1);
+            assert_eq!(window.transactions_by_hash.len(), i + 1);
+
+            let result = window.throttle_check(now);
+            assert_eq!(result, Ok(()));
+        }
+
+        // push new transaction
+        let block_height = BigUint::from(push_txs_count + 1);
+        let block_timestamp = now + 1;
+        let tx_info: TransactionInfo = TransactionInfo {
+            block_timestamp: block_timestamp.clone(),
+            tx_hash: compute_hash(format!("test{}", block_height).as_bytes()),
+        };
+        window.push_transaction(block_height.clone(), tx_info.clone());
+
+        assert_eq!(window.transactions_by_height.len(), push_txs_count + 1);
+        assert_eq!(window.transactions_by_hash.len(), push_txs_count + 1);
+
+        // check throttling
+        let result = window.throttle_check(now);
+        assert_eq!(
+            result,
+            Err(DFTError::TooManyTransactionsInReplayPreventionWindow)
+        );
+    }
+
+    #[test]
+    fn test_decode_encode() {
+        let mut window = TokenTransactionWindow::new();
+        let push_txs_count = 100;
+
+        // get now timestamp
+        let now: u64 = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_nanos()
+            .try_into()
+            .unwrap();
+
+        for i in 0..push_txs_count {
+            let block_height = BigUint::from(i as u32);
+            let block_timestamp = now - push_txs_count as u64 + 1;
+            let tx_info: TransactionInfo = TransactionInfo {
+                block_timestamp: block_timestamp.clone(),
+                tx_hash: compute_hash(format!("test{}", i).as_bytes()),
+            };
+            window.push_transaction(block_height.clone(), tx_info.clone());
+            assert_eq!(window.transactions_by_height.len(), i + 1);
+            assert_eq!(window.transactions_by_hash.len(), i + 1);
+        }
+
+        let encoded = window.encode();
+        let decoded = TokenTransactionWindow::decode(encoded);
+        assert_eq!(decoded.unwrap(), window);
     }
 }
