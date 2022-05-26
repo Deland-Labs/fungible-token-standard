79e17f6be78c8467966e418fc158fe6381fc38ae
witter-deland <87846830+witter-deland@users.noreply.github.com>
2022-05-26 15:01:52 +0800

    Improve: notify (#132)

--- src/dft_basic/src/canister_api/transfer_notify.rs
+++ src/dft_basic/src/canister_api/transfer_notify.rs
@@ -1,10 +1,10 @@
 use candid::{Nat, Principal};
-use dft_types::{TokenAmount, TokenHolder};
+use dft_types::{BlockHeight, TokenAmount, TokenHolder};
 use dft_utils::principal::is_canister;
 use log::{debug, info, warn};
 
 pub trait ITransferNotifyAPI {
-    fn notify(&self, receiver: &String, transfer_from: &TokenHolder, transfer_value: &TokenAmount);
+    fn notify(&self, receiver: &String, block_height: &BlockHeight, transfer_from: &TokenHolder, transfer_value: &TokenAmount);
 }
 
 #[derive(Default)]
@@ -12,25 +12,27 @@ pub struct TransferNotifyAPI;
 
 #[cfg_attr(coverage_nightly, no_coverage)]
 impl ITransferNotifyAPI for TransferNotifyAPI {
-    fn notify(&self, receiver: &String, transfer_from: &TokenHolder, transfer_value: &TokenAmount) {
+    fn notify(&self, receiver: &String, block_height: &BlockHeight, transfer_from: &TokenHolder, transfer_value: &TokenAmount) {
         let pid = Principal::from_text(receiver);
         debug!("TransferNotifyAPI::notify in");
         if let Ok(receiver_canister_id) = pid {
             if is_canister(&receiver_canister_id) {
-                info!("TransferNotifyAPI::notify:  will notify receiver: {:?},transfer_from is {},transfer_value is {}", receiver_canister_id.to_text(),transfer_from.clone().to_hex(),transfer_value.clone());
+                info!("TransferNotifyAPI::notify:  will notify receiver: {:?},height is {},transfer_from is {},transfer_value is {}",
+                    receiver_canister_id.to_text(),block_height,transfer_from.clone().to_hex(),transfer_value.clone());
                 //notify receiver
+                let nat_block_height: Nat = block_height.clone().into();
                 let nat_transfer_value: Nat = transfer_value.clone().into();
                 ic_cdk::notify(
                     receiver_canister_id.clone(),
                     "onTokenReceived",
-                    (transfer_from.clone(), nat_transfer_value,),
+                    (nat_block_height, transfer_from.clone(), nat_transfer_value, ),
                 )
-                .unwrap_or_else(|reject| {
-                    warn!(
+                    .unwrap_or_else(|reject| {
+                        warn!(
                         "failed to notify (receiver_canister_id={}): {:?}",
                         receiver_canister_id, reject
                     )
-                });
+                    });
             }
         };
     }
